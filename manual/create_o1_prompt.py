d089d36952fb884ae23780d696c1883929df6af6
sakastudio <katsumi.sato@aktsk.jp>
2024-10-13 18:16:46 +0900

    auto update embedetting

--- manual/create_o1_prompt.py
+++ manual/create_o1_prompt.py
@@ -2,10 +2,12 @@ from chat_bot import ask
 from mooresmaster import master_rag_prompt
 from util import embedding
 
-# master.txtをロード
-usr_prompt = "MinerMiningTestクラスを参考に、VanillaGearMinerComponentのテストコードを記述してください。"
+# ベクトル化されたファイルを更新
+embedding.update_embedding()
 
-prompt = embedding.create_rag_prompt(usr_prompt, token_limit=20000)
+usr_prompt = "他のSaveLoadTestを参考に、ElectricMinerとGearMinerのSaveLoadTestを作成してください。"
+
+prompt = embedding.create_rag_prompt(usr_prompt, token_limit=24000)
 
 prompt += "\n# System Prompt\n" + ask.SYSTEM_PROMPT + "\n# Instructions\n" + usr_prompt
 
